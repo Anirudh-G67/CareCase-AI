326d8ed833b35bb487eb089f6ebac1014c103d26
Anirudh Goel <ani.rushu3023@gmail.com>
2026-09-15 13:57:16 +0530

    Again updated frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,15 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://carecase-ai-j4wy.onrender.com',
-        changeOrigin: true,
-        secure: false,
-        // Optional: if your FastAPI endpoints do not include '/api' prefix, rewrite it:
-        // rewrite: (path) => path.replace(/^\/api/, '')
-      },
-    },
-  },
-})
+ })
